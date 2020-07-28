e73e51b67266caf8bf1a92f76b801ca2d82d46c5
thigarampratap <68469552+thigarampratap@users.noreply.github.com>
2020-07-28 22:33:23 +0530

    V2.1

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-print('-' * 50)
+print('-' * 51)
 print('Current python version : ', sys.version)
 print('Current python work directory:', os.getcwd())
-print('-' * 50)
+print('-' * 51)
